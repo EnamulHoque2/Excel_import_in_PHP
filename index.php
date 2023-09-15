d08990911ed9e9ceac3e2b1417f0c68c6163749f
EnamulHoque2 <130828707+EnamulHoque2@users.noreply.github.com>
2023-09-15 14:23:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,39 +18,39 @@
 		foreach($obj->getWorksheetIterator() as $sheet){
 			$getHighestRow=$sheet->getHighestRow();
 			for($i=2;$i<=$getHighestRow;$i++){
-				$stu_name=$sheet->getCellByColumnAndRow(0,$i)->getValue();
-				$father=$sheet->getCellByColumnAndRow(1,$i)->getValue();
-				$mother=$sheet->getCellByColumnAndRow(2,$i)->getValue();
-				$reg_no=$sheet->getCellByColumnAndRow(3,$i)->getValue();
-				$mobile=$sheet->getCellByColumnAndRow(4,$i)->getValue();
-				$product=$sheet->getCellByColumnAndRow(5,$i)->getValue();
-				$session=$sheet->getCellByColumnAndRow(6,$i)->getValue();
-				$course=$sheet->getCellByColumnAndRow(7,$i)->getValue();
-				$email=$sheet->getCellByColumnAndRow(8,$i)->getValue();
-				$class=$sheet->getCellByColumnAndRow(9,$i)->getValue();
-				$amount=$sheet->getCellByColumnAndRow(10,$i)->getValue();
-				$trn=$sheet->getCellByColumnAndRow(11,$i)->getValue();
+				$col1=$sheet->getCellByColumnAndRow(0,$i)->getValue();
+				$col2=$sheet->getCellByColumnAndRow(1,$i)->getValue();
+				$col3=$sheet->getCellByColumnAndRow(2,$i)->getValue();
+				$col4=$sheet->getCellByColumnAndRow(3,$i)->getValue();
+				$col5=$sheet->getCellByColumnAndRow(4,$i)->getValue();
+				$col6=$sheet->getCellByColumnAndRow(5,$i)->getValue();
+				$col7=$sheet->getCellByColumnAndRow(6,$i)->getValue();
+				$col8=$sheet->getCellByColumnAndRow(7,$i)->getValue();
+				$col9=$sheet->getCellByColumnAndRow(8,$i)->getValue();
+				$col10=$sheet->getCellByColumnAndRow(9,$i)->getValue();
+				$col11=$sheet->getCellByColumnAndRow(10,$i)->getValue();
+				$col12=$sheet->getCellByColumnAndRow(11,$i)->getValue();
 				if(PHPExcel_Shared_Date::isDateTime($sheet->getCellByColumnAndRow(12,$i))) {
 				    $pdate1Value = $sheet->getCellByColumnAndRow(12,$i)->getValue();
 				    $pdate1timestamp = PHPExcel_Shared_Date::ExcelToPHP($pdate1Value);
-				    $pdate= date('Y-m-d', $pdate1timestamp);
+				    $col13= date('Y-m-d', $pdate1timestamp);
 				}
-				$pstatus=$sheet->getCellByColumnAndRow(13,$i)->getValue();
-				$pmode=$sheet->getCellByColumnAndRow(14,$i)->getValue();
-				$gatetrn=$sheet->getCellByColumnAndRow(15,$i)->getValue();
-				$paction=$sheet->getCellByColumnAndRow(16,$i)->getValue();
-				$dept_roll=$sheet->getCellByColumnAndRow(17,$i)->getValue();
-				$gender=$sheet->getCellByColumnAndRow(18,$i)->getValue();
+				$col14=$sheet->getCellByColumnAndRow(13,$i)->getValue();
+				$col15=$sheet->getCellByColumnAndRow(14,$i)->getValue();
+				$col16=$sheet->getCellByColumnAndRow(15,$i)->getValue();
+				$col17=$sheet->getCellByColumnAndRow(16,$i)->getValue();
+				$col18=$sheet->getCellByColumnAndRow(17,$i)->getValue();
+				$col19=$sheet->getCellByColumnAndRow(18,$i)->getValue();
 				if(PHPExcel_Shared_Date::isDateTime($sheet->getCellByColumnAndRow(19,$i))) {
 				    $dobValue = $sheet->getCellByColumnAndRow(19,$i)->getValue();
 				    $dobimestamp = PHPExcel_Shared_Date::ExcelToPHP($dobValue);
-				    $dob= date('Y-m-d', $dobimestamp);
+				    $col20= date('Y-m-d', $dobimestamp);
 				}
-				$category=$sheet->getCellByColumnAndRow(20,$i)->getValue();
+				$col30=$sheet->getCellByColumnAndRow(20,$i)->getValue();
 				
 
 				if($stu_name!=''){
-					mysqli_query($con,"insert into payment(sname,f_name,m_name,reg_no,mob,product,session,course,email,class,amount,trn,pdate,pstatus,pmode,gatetrn,paction,dept_roll,gen,dob,cat) values('$stu_name','$father','$mother','$reg_no','$mobile','$product','$session','$course','$email','$class','$amount','$trn','$pdate','$pstatus','$pmode','$gatetrn','$paction','$dept_roll','$gender','$dob','$category')");
+					mysqli_query($con,"insert into tablename(col1,col2,col3,col4,col5,col6,session,course,email,class,amount,trn,pdate,pstatus,pmode,gatetrn,paction,dept_roll,gen,dob,cat) values('$stu_name','$father','$mother','$reg_no','$mobile','$product','$session','$course','$email','$class','$amount','$trn','$pdate','$pstatus','$pmode','$gatetrn','$paction','$dept_roll','$gender','$dob','$category')");
 				}
 			}
 		}
